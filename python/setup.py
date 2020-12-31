8fb64245c118fb9d48c983392bababc1baf3d9dc
Lucas Marie <twitchfordoog@gmail.com>
2020-12-31 02:13:00 +0100

    Refactoring for pip

--- python/setup.py
+++ python/setup.py
@@ -1,10 +1,20 @@
 from setuptools import setup
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
 setup(name='UniSerializer',
-version='1.0',
-description='Fast and efficient universal data serializer',
-url='https://github.com/Diffylab/UniSerializer',
-author='Diffy',
-author_email='diffylab@gmail.com',
-license='MIT',
-packages=['.'],
-zip_safe=False)
+    version='1.0',
+    description='Fast and efficient universal data serializer',
+    url='https://github.com/Diffylab/UniSerializer',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='Diffy',
+    author_email='diffylab@gmail.com',
+    license='MIT',
+    packages=['pkg'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Topic :: Software Development :: Libraries"
+        "Operating System :: OS Independent",
+    ],
+)

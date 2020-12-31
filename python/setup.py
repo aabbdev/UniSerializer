842e64f1ecb21effa729222d6e6f077337615d24
aabbdev <twitchfordoog@gmail.com>
2020-12-31 02:23:41 +0100

    Refactoring for multi-languages

--- python/setup.py
+++ python/setup.py
@@ -1,16 +1,12 @@
 from setuptools import setup
-with open("../README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
 setup(name='uniserializer',
     version='1.0',
     description='Fast and efficient universal data serializer',
     url='https://github.com/Diffylab/UniSerializer',
-    long_description=long_description,
-    long_description_content_type="text/markdown",
     author='Diffy',
     author_email='diffylab@gmail.com',
     license='MIT',
-    packages=['pkg'],
+    packages=['src'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",

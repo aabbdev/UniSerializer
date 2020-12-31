3cc2c4551f7bf4b68210b100752617255b5a70a9
aabbdev <twitchfordoog@gmail.com>
2020-12-31 02:15:36 +0100

    Refactoring for pip

--- python/setup.py
+++ python/setup.py
@@ -1,5 +1,5 @@
 from setuptools import setup
-with open("README.md", "r", encoding="utf-8") as fh:
+with open("../README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 setup(name='UniSerializer',
     version='1.0',

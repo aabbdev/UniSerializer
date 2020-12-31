fad5f2aff72e91ee3c3164f4f173c1ded4224711
aabbdev <twitchfordoog@gmail.com>
2020-12-31 02:45:35 +0100

    Pip..

--- python/setup.py
+++ python/setup.py
@@ -1,16 +1,17 @@
 from setuptools import setup
 setup(name='uniserializer',
-    version='1.0',
+    version='1.1',
     description='Fast and efficient universal data serializer',
     url='https://github.com/Diffylab/UniSerializer',
     author='Diffy',
     author_email='diffylab@gmail.com',
+    download_url = 'https://github.com/Diffylab/UniSerializer/releases/',
+    keywords = ['Serializer', 'Deserializer', 'data', 'binary', 'encoding'],
     license='MIT',
     packages=['src'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Topic :: Software Development :: Libraries"
-        "Operating System :: OS Independent",
     ],
 )

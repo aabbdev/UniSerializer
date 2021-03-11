3bb163ebc59609c1a9cb7c0946579645cf9d5a65
aabbdev <twitchfordoog@gmail.com>
2021-03-11 03:51:36 +0100

    [NEW] Single header

--- c/examples/example1.c
+++ c/examples/example1.c
@@ -6,7 +6,7 @@ int main()
    /* printf function displays the content that is
     * passed between the double quotes.
     */
-    UniBuffer buffer = Uni_init(64, true);
+    UniBuffer buffer = Uni_init(64, true, true);
     printf("%d\n", (int)buffer.length);
 
     printf("Hello World\n");
